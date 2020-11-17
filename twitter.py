db202634f267ee95aad546850fe0269e6b3e2c4b
John Paul Batusan <johnpaul.batusa@bicol-u.edu.ph>
2020-11-17 17:18:06 +0800

    Update twitter code from Barbas code

--- twitter.py
+++ twitter.py
@@ -1,74 +1,37 @@
 import tweepy
 import time
-import os
-from os import environ
+from os import environ 
+
 
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-
 auth.set_access_token(ACCESS_KEY,ACCESS_SECRET)
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
-user = api.me()
+numberOfTweets = 100
 
-Crypto_Shooter = api.get_user("Crypto_Shooter")
-CryptoBurstGame = api.get_user("CryptoBurstGame")
-CryptoSlicer = api.get_user("CryptoSlicer")
+accountsToFollow = ['CryptoBurstGame','Crypto_Shooter','SparkDeFi','CryptoSlicer','HydroBlockchain','sparkpointio','JRNYcrypto']
 
-sparkPoint = api.get_user("sparkpointio")
+for user_name in itertools.cycle(accountsToFollow):
+    user = api.get_user(screen_name = user_name)
+    print(user.screen_name)
 
-timeline = sparkPoint.timeline()
+    for tweet in tweepy.Cursor(api.user_timeline, id=user.screen_name).items(numberOfTweets):
+        try:
+            if not tweet.retweeted:
+                tweet.favorite()
+                print('Tweet Liked!')
+                tweet.retweet()
+                print('Tweet Retweeted!')
+                time.sleep(10)
+                break
 
-while(True):
-    for tweetSparkIo in timeline:
-        if not tweetSparkIo.favorited:
-            try:
-                api.create_favorite(tweetSparkIo.id)
-            except Exception as e:
-                print(e.reason)
-        if not tweetSparkIo.retweeted:
-            try:
-                tweetSparkIo.retweet()
-            except Exception as e:
-                print(e.reason)
-    for tweetShooter in timeline:
-        if not tweetShooter.favorited:
-            try:
-                api.create_favorite(tweetShooter.id)
-            except Exception as e:
-                print(e.reason)
-        if not tweetShooter.retweeted:
-            try:
-                tweetShooter.retweet()
-            except Exception as e:
-                print(e.reason)
-    for tweetBurst in timeline:
-        if not tweetBurst.favorited:
-            try:
-                api.create_favorite(tweetBurst.id)
-            except Exception as e:
-                print(e.reason)
-        if not tweetBurst.retweeted:
-            try:
-                tweetBurst.retweet()
-            except Exception as e:
-                print(e.reason)
-    for tweetSlicer in timeline:
-        if not tweetSlicer.favorited:
-            try:
-                api.create_favorite(tweetSlicer.id)
-            except Exception as e:
-                print(e.reason)
-        if not tweetSlicer.retweeted:
-            try:
-                tweetSlicer.retweet()
-            except Exception as e:
-                print(e.reason)
-
-
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
