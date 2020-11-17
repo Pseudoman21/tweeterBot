8daf6cd7e0226d281dbc76d062d6577bbfc4b3ba
John Paul Batusan <johnpaul.batusa@bicol-u.edu.ph>
2020-11-17 16:37:59 +0800

    Make the script run infinitly

--- twitter.py
+++ twitter.py
@@ -25,52 +25,50 @@ sparkPoint = api.get_user("sparkpointio")
 
 timeline = sparkPoint.timeline()
 
-for tweetSparkIo in timeline:
-    if not tweetSparkIo.favorited:
-    	try:
-    		api.create_favorite(tweetSparkIo.id)
-    	except Exception as e:
-    		logger.error("Error on fav", exc_info=True)
-    if not tweetSparkIo.retweeted:
-    	try:
-    		tweetSparkIo.retweet()
-    	except Exception as e:
-    		logger.error("Error on fav and retweeted", exc_info=True)
-
-for tweetShooter in timeline:
-    if not tweetShooter.favorited:
-        try:
-            api.create_favorite(tweetShooter.id)
-        except Exception as e:
-            logger.error("Error on fav", exc_info=True)
-    if not tweetShooter.retweeted:
-        try:
-            tweetShooter.retweet()
-        except Exception as e:
-            logger.error("Error on fav and retweeted", exc_info=True)
-
-for tweetBurst in timeline:
-    if not tweetBurst.favorited:
-        try:
-            api.create_favorite(tweetBurst.id)
-        except Exception as e:
-            logger.error("Error on fav", exc_info=True)
-    if not tweetBurst.retweeted:
-        try:
-            tweetBurst.retweet()
-        except Exception as e:
-            logger.error("Error on fav and retweeted", exc_info=True)
-
-for tweetSlicer in timeline:
-    if not tweetSlicer.favorited:
-        try:
-            api.create_favorite(tweetSlicer.id)
-        except Exception as e:
-            logger.error("Error on fav", exc_info=True)
-    if not tweetSlicer.retweeted:
-        try:
-            tweetSlicer.retweet()
-        except Exception as e:
-            logger.error("Error on fav and retweeted", exc_info=True)
+while(True):
+    for tweetSparkIo in timeline:
+        if not tweetSparkIo.favorited:
+            try:
+                api.create_favorite(tweetSparkIo.id)
+            except Exception as e:
+                logger.error("Error on fav", exc_info=True)
+                if not tweetSparkIo.retweeted:
+                    try:
+                        tweetSparkIo.retweet()
+                    except Exception as e:
+                        logger.error("Error on fav and retweeted", exc_info=True)
+    for tweetShooter in timeline:
+        if not tweetShooter.favorited:
+            try:
+                api.create_favorite(tweetShooter.id)
+            except Exception as e:
+                logger.error("Error on fav", exc_info=True)
+        if not tweetShooter.retweeted:
+            try:
+                tweetShooter.retweet()
+            except Exception as e:
+                logger.error("Error on fav and retweeted", exc_info=True)
+    for tweetBurst in timeline:
+        if not tweetBurst.favorited:
+            try:
+                api.create_favorite(tweetBurst.id)
+            except Exception as e:
+                logger.error("Error on fav", exc_info=True)
+                if not tweetBurst.retweeted:
+            try:
+                tweetBurst.retweet()
+            except Exception as e:
+                logger.error("Error on fav and retweeted", exc_info=True)
+    for tweetSlicer in timeline:
+        if not tweetSlicer.favorited:
+            try:
+                api.create_favorite(tweetSlicer.id)
+            except Exception as e:
+                logger.error("Error on fav", exc_info=True)
+        if not tweetSlicer.retweeted:
+            try:
+                tweetSlicer.retweet()
+            except Exception as e:
+                logger.error("Error on fav and retweeted", exc_info=True)
 
 
